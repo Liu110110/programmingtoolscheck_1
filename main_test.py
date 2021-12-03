7a7a0a0515a587ac612b60224661cc19eb21eeb7
unknown <yuqiliu@163.com>
2021-12-03 12:26:54 +0800

    I finished my homework

--- main_test.py
+++ main_test.py
@@ -1,5 +1,5 @@
 import unittest
-from main import print_hi_stephen, print_hi_you#, print_hi_?
+from main import print_hi_stephen, print_hi_you, print_hi_michael
 
 
 class MyTestCase(unittest.TestCase):
@@ -7,7 +7,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(print_hi_stephen(), f"Hi, Stephen")
 
     def test_print_hi_you(self):
-        self.assertEqual(print_hi_you(), f"")
+        self.assertEqual(print_hi_you(), f"Hi, Tony")
+
+    def test_print_hi_michael(self):
+        self.assertEqual(print_hi_michael(), f"Hi, Michael")
 
     #def test_print_hi_?(self):
         #
